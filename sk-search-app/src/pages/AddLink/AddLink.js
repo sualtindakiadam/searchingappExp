99744806fac6497042d4c3a2bf5f1e1dde5409f5
Semih <31005189+sualtindakiadam@users.noreply.github.com>
2022-11-10 15:59:47 +0300

    10/11/22 16.00

--- sk-search-app/src/pages/AddLink/AddLink.js
+++ sk-search-app/src/pages/AddLink/AddLink.js
@@ -8,15 +8,47 @@ import Button from "../../components/Button";
 import TesodevLogo from "../../components/TesodevLogo";
 
 
+
+
 const titles = ([
-    { id: 1, title: "Name Surname" },
-    { id: 2, title: "Country" },
-    { id: 3, title: "City" },
-    { id: 4, title: "EMail" },
+    { id: 1, title: "Name Surname", minChar: "4", maxChar: "60", inputType: "text" },
+    { id: 2, title: "Country", minChar: "2", maxChar: "40", inputType: "text" },
+    { id: 3, title: "City", minChar: "2", maxChar: "40", inputType: "text" },
+    { id: 4, title: "EMail", minChar: "4", maxChar: "60", inputType: "email" },
 
 ])
 
 export default function AddLink() {
+
+
+    const addClick = () => {
+        alert("added")
+    }
+    const inputType = (event, type) => {
+        if (type == "text") {
+
+            if (!/^[A-Za-z]+$/.test(event.key)) {
+                event.preventDefault();
+            }
+
+        } else if (type == "number") {
+            if (!/[0-9]/.test(event.key)) {
+                event.preventDefault();
+            }
+        }
+    }
+
+
+    emailValidation(){
+        const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        /*if(!this.state.email || regex.test(this.state.email) === false){
+            this.setState({
+                error: "Email is not valid"
+            });
+            return false;
+        }
+        return true;*/
+    }
 
 
     return (
@@ -29,19 +61,31 @@ export default function AddLink() {
                 </NavLink>
             </div>
             <div className="formContainer">
-                <div>
+                <div >
                     {titles.map((d) => {
                         return <div className="formItemContainer" >
                             <div >{d.title}</div>
                             <div >
-                                <input type='text' className="input" onChange={(e) => console.log(d.title)} placeholder={"Enter " + d.title} />
+                                <input
+                                    type={d.inputType}
+                                    className="input"
+                                    onChange={(e) => console.log(d.title)}
+                                    placeholder={"Enter " + d.title}
+                                    onKeyPress={(event) => inputType(event, d.inputType)}
+                                />
                             </div>
                         </div>
                     })}
+                    <div className="addContainer">
+                        <Button text="Add" func={addClick} />
+
+                    </div>
                 </div>
 
             </div>
-      
+
+
+
 
         </div>
     )
